27e2fd3abb33906751900cda2a842114322eafc9
247813 Tomalak Piotr <247813@edu.p.lodz.pl>
2024-11-13 22:36:55 +0100

    Sheep class + wolf class (wolf class to consideration)

---
+++ sheep.py
@@ -0,0 +1,20 @@
+import random
+
+from animal import Animal
+
+
+class Sheep(Animal):
+    def __init__(self, x, y, move_distance=0.5):
+        super().__init__(x, y)
+        self.move_distance = move_distance
+
+    def move(self):
+        directions = {
+            "north": (0, self.move_distance),
+            "south": (0, -self.move_distance),
+            "east": (self.move_distance, 0),
+            "west": (-self.move_distance, 0)
+        }
+        direction = random.choice(list(directions.values()))
+        x, y = self.position
+        self.position = (x + direction[0], y + direction[1])
